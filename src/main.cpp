2357e240658d4bef8fce54fc8477a5e13f052afc
ywang <ywang@vewd.com>
2022-10-23 12:01:35 +0800

    1.introduce encode   1-1.avcodec_find_encode   1-2.avcodec_alloc_context3 & avcodec_free_context   1-3.avcodec_open2 2.Build by using pre-installed the ffmpeg package. 3.Back old CMakeList.txt of using self-compiled ffmpeg package

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@ int main() {
   std::cout << "==2. prepare converting format" << inUrl << std::endl;
   int nSrcWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
   int nSrcHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+  int nFPS = cap.get(cv::CAP_PROP_FPS);
   SwsContext* pSct = NULL;
   pSct = sws_getCachedContext(pSct, nSrcWidth, nSrcHeight, AV_PIX_FMT_BGR24, nSrcWidth, nSrcHeight, AV_PIX_FMT_YUV420P, SWS_BICUBIC, 0, 0, 0);
   if(!pSct) {
@@ -45,32 +46,61 @@ int main() {
   pYUVFrame->width = nSrcWidth;
   pYUVFrame->height = nSrcHeight;
   pYUVFrame->pts = 0;
-  int res = av_frame_get_buffer(pYUVFrame, 32);
+  std::cout << "pYUVFrame->width = " << pYUVFrame->width << ", pYUVFrame->height = " << pYUVFrame->height << std::endl;
+  int res = av_frame_get_buffer(pYUVFrame, 0);
   if(res != 0) {
     return XError(res);
   }
 
-  std::cout << "==3. process frame loop begin.. " << std::endl;
+  std::cout << "==3. encode.. " << std::endl;
+  const AVCodec* pH264Codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+  if(!pH264Codec) {
+    std::cout << "avcodec_find_encoder failed" << std::endl;
+    exit(0);
+  }
+  AVCodecContext* pAvCodecContext = avcodec_alloc_context3(pH264Codec);
+  if(!pAvCodecContext) {
+    std::cout << "avcodec_alloc_context3 failed" << std::endl;
+    exit(0);
+  }
+  pAvCodecContext->flags |= AV_CODEC_FLAG_GLOBAL_HEADER; //
+  pAvCodecContext->codec_id = pH264Codec->id;
+  pAvCodecContext->thread_count = 8;
+  pAvCodecContext->bit_rate = 50 * 1024 * 8; // sampling video 50kB/sec
+  pAvCodecContext->width = nSrcWidth;
+  pAvCodecContext->height = nSrcHeight;
+  pAvCodecContext->time_base = {1, nFPS};
+  pAvCodecContext->framerate = {nFPS, 1};
+  pAvCodecContext->gop_size = 50; //how many frames contains 1 key frame.
+  pAvCodecContext->max_b_frames = 0; // workaround for simplifying dts
+  pAvCodecContext->pix_fmt = AV_PIX_FMT_YUV420P;
+
+  res = avcodec_open2(pAvCodecContext, 0, 0);
+  if(res != 0) {
+    return XError(res);
+  }
+
+  std::cout << "==4. process frame loop begin.. " << std::endl;
   while (true) {
     if(!cap.grab()) { // read and decode
       std::cout << "cap.grab() fail!" << std::endl;
       continue;
     }
-    cv::Mat frame;
-    if(!cap.retrieve(frame)) { // output frame
+    cv::Mat inFrame;
+    if(!cap.retrieve(inFrame)) { // output inFrame
       std::cout << "cap.retrieve() fail!" << std::endl;
       continue;
     }
-    cv::imshow("RTSP", frame);   
+    cv::imshow("RTSP", inFrame);   
     cv::waitKey(1);
 
     // RGB to YUV, now RGB
     uint8_t *inData[AV_NUM_DATA_POINTERS] = {0};
     //BGRBGR..
-    inData[0] = frame.data;
-    int inSize[AV_NUM_DATA_POINTERS] = {0};
-    inSize[0] = frame.cols * frame.elemSize();
-    int h = sws_scale(pSct, inData, inSize, 0, frame.rows, pYUVFrame->data, pYUVFrame->linesize);
+    inData[0] = inFrame.data;
+    int inStride[AV_NUM_DATA_POINTERS] = {0};
+    inStride[0] = inFrame.cols * inFrame.elemSize();
+    int h = sws_scale(pSct, inData, inStride, 0, inFrame.rows, pYUVFrame->data, pYUVFrame->linesize);
     if(h <= 0) {
       continue;
     }
@@ -82,6 +112,10 @@ int main() {
 
   av_frame_free(&pYUVFrame);
   pYUVFrame = NULL;
+
+  if(pAvCodecContext) {
+    avcodec_free_context(&pAvCodecContext);
+  }
   
   return 0;
 }
